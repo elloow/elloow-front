9feb922b456298efa8e7dc73d67295686e305118
CreB <bastien.crettenand@hotmail.com>
2020-11-02 17:44:04 +0100

    Add basic layout

---
+++ components/Layout/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-navbar type="is-primary">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="~/assets/elloow.png"
+          alt="Elloow"
+        >
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <nuxt-link to="/log-in">
+            <a class="button is-primary">
+              <strong>Log In</strong>
+            </a>
+          </nuxt-link>
+          <nuxt-link to="/sign-up">
+            <a class="button is-primary is-light">
+              <strong>Sign Up</strong>
+            </a>
+          </nuxt-link>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <CThemeProvider>
-    <CReset />
-    <Nuxt />
-  </CThemeProvider>
+  <div class="container">
+    <b-skeleton width="20%" :animated="animated" />
+    <b-skeleton width="60%" :animated="animated" />
+    <b-skeleton width="40%" :animated="animated" />
+    <b-skeleton width="10%" :animated="animated" />
+    <b-skeleton width="80%" :animated="animated" />
+    <b-skeleton :animated="animated" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-
 })
 </script>
