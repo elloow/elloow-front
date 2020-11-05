ab06e6fc15cbc65ce12298713ff33b13f3ff028e
CreB <bastien.crettenand@hotmail.com>
2020-11-05 12:26:39 +0100

    Add basic application structure

---
+++ pages/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>
+    Admin page
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
---
+++ pages/organisations/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>
+    Organisations
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
