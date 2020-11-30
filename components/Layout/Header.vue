ad505b12247442d5eb0cb7dccfed437c97faeadd
crbast <bastien.crettenand@hotmail.com>
2020-11-30 15:56:30 +0000

    Fix modern build UI errors

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -12,15 +12,11 @@
     <template slot="end">
       <b-navbar-item tag="div">
         <div class="buttons">
-          <nuxt-link to="/login">
-            <a class="button is-primary">
-              <strong>Log In</strong>
-            </a>
+          <nuxt-link to="/login" class="button is-primary">
+            <strong>Log In</strong>
           </nuxt-link>
-          <nuxt-link to="/get-started">
-            <a class="button is-primary is-light">
-              <strong>Get started</strong>
-            </a>
+          <nuxt-link to="/get-started" class="button is-primary is-light">
+            <strong>Get started</strong>
           </nuxt-link>
         </div>
       </b-navbar-item>
