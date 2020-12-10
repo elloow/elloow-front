98e3e57da58280198a77538dd88e6e8e37b31d62
crbast <bastien.crettenand@hotmail.com>
2020-12-10 15:40:52 +0000

    Add basic organisation page

---
+++ pages/organisations/_name.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Organisation name : {{ org_name }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  middleware: ['auth'],
+  data () {
+    return {
+      org_name: '' as string
+    }
+  },
+  async mounted () {
+    try {
+      const data = (await this.$axios.get(`/v1/organisations/${this.$route.params.name}`)).data
+      this.org_name = data.data.name
+    } catch (error) { }
+  }
+})
+</script>
