044b8089397a1031f66ff2da43a4b212184ac13a
crbast <bastien.crettenand@hotmail.com>
2021-10-15 13:45:34 +0200

    Base nuxt 3 setup

--- plugins/vee-validate.ts
+++
@@ -1,9 +0,0 @@
-import { extend } from 'vee-validate'
-import * as rules from 'vee-validate/dist/rules'
-import { ValidationRule } from 'vee-validate/dist/types/types'
-
-for (const [rule, validation] of Object.entries(rules)) {
-  extend(rule, {
-    ...validation as ValidationRule
-  })
-}
